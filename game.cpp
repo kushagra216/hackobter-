3552f28946dc5375e4d059801f52ad9915d57ed0
kushagrasharma217 <73079668+kushagrasharma217@users.noreply.github.com>
2020-10-19 01:56:41 +0530

    Update game.cpp
    inlined the code 
    added the commments
    increased readibility
    efficiency increased

--- game.cpp
+++ game.cpp
@@ -1,8 +1,9 @@
-#include <iostream> 
+#include <iostream.h> 
 #include<stdlib.h>
 #include<time.h>
 using namespace std;
-int main(){
+int main()
+      {
       int number,guess,attempt=0;
       srand(time(0));
             number=rand()%100 + 1;
@@ -10,18 +11,18 @@ int main(){
             {
                 cout<<"\t\t\tGuess the number between 1 and 100\n";
                 cin>>guess;
-                if (guess<number)
+                if (guess<number)                               //started the loop
                 {
                   cout<<"\t\t\tGuess a little higher\n";
-                  attempt++;
+                  attempt++;                                    //increamenting attempts
                 }
-                else if (guess>number)
+                else if (guess>number)                          //checking the second condition
                 {
                   cout<<"\t\t\tGuess a bit lower\n"; 
-                   attempt++;               }
+                   attempt++;               }                   //increamenting attempts
                 else
                 {          
-                     cout<<"\t\t\tYou have guessed the number right shabassh you are still my kaddu\nYou scored  "<<attempt<<" points";
+                     cout<<"\t\t\tYou have guessed the number right shabassh you are still my kaddu\nYou scored  "<<attempt<<" points"<<endl;
                     }
                   
             } while (number!=guess);
